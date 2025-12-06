7875c7c34b622da06f7f2c04705dae6e7a292628
linconjbrito <linconjbrito@gmail.com>
2025-12-05 21:25:17 -0300

    retirei supase_options do ignore

---
+++ lib/supabase_options.dart
@@ -0,0 +1,9 @@
+
+
+
+const Map<Symbol, dynamic> supabaseOptions = {
+  #url: 'https://xuhjgbptwphsvxriyqey.supabase.co',
+  #anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1aGpnYnB0d3Boc3Z4cml5cWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMDkxMjYsImV4cCI6MjA3OTY4NTEyNn0.WO5Sp20v8uZn7gcD_xX7y7V43JeFmgImZQZ3CMzfyeo',
+};
+
+
